525f7341bcf77aef7b3ce9114b391641e66adf3d
James Halliday <james.halliday@gmail.com>
2026-06-20 16:53:14 +0100

    Happy with pattern to delegate from menu via actions

--- app/actions.py
+++ app/actions.py
@@ -1,3 +1,13 @@
+##File/module/whatever you're supposed to call them that handles incoming actions from the user menu.
+##Small bits of logic can stay in here, but larger pieces will just call a dedicated module
+
+from app.add_RSS import add_RSS
+
+def call_add_RSS():
+    add_RSS()
+
+
+
 ##First very simple action - just trigger this to close the app
 def exit_app():
     print("Thank you. Come again!")
---
+++ app/add_RSS.py
@@ -0,0 +1,2 @@
+def add_RSS():
+    print("Here's where we'll add a feed later")
